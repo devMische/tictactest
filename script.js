c420ac89ca95a87f66661f89d72744dc33d4274f
DerMische <ehcsimred@web.de>
2024-12-07 11:51:16 +0100

    svg change tested..

--- script.js
+++ script.js
@@ -2,18 +2,16 @@ var player1 = true;
 var playing = true;
 var round = 0;
 
-
-const tictactoe = document.getElementById('tictactoe');
-const i1 = document.getElementById('i1');
-const i2 = document.getElementById('i2');
-const i3 = document.getElementById('i3');
-const i4 = document.getElementById('i4');
-const i5 = document.getElementById('i5');
-const i6 = document.getElementById('i6');
-const i7 = document.getElementById('i7');
-const i8 = document.getElementById('i8');
-const i9 = document.getElementById('i9');
-
+const tictactoe = document.getElementById("tictactoe");
+const i1 = document.getElementById("i1");
+const i2 = document.getElementById("i2");
+const i3 = document.getElementById("i3");
+const i4 = document.getElementById("i4");
+const i5 = document.getElementById("i5");
+const i6 = document.getElementById("i6");
+const i7 = document.getElementById("i7");
+const i8 = document.getElementById("i8");
+const i9 = document.getElementById("i9");
 
 i1.addEventListener("click", setSign);
 i2.addEventListener("click", setSign);
@@ -25,149 +23,151 @@ i7.addEventListener("click", setSign);
 i8.addEventListener("click", setSign);
 i9.addEventListener("click", setSign);
 
-
-
 function setSign(e) {
+  round += 1;
 
-    round += 1;
+  if (playing == false || round > 9) {
+    reset();
+  } else {
+    var element = e.target || e.srcElement;
 
-    if(playing == false || round >  9){
-        reset();
-    } else {
+    if (document.getElementById(element.id).classList.length < 2 && playing) {
+      if (player1) {
+        document.getElementById(element.id).classList.add("p1");
+        player1 = false;
+      } else {
+        document.getElementById(element.id).classList.add("p2");
+        player1 = true;
+      }
 
-        var element = e.target || e.srcElement;
-
-        if(document.getElementById(element.id).classList.length < 2 && playing){
-            
-            if(player1) {
-                document.getElementById(element.id).classList.add('p1');
-                player1 = false;
-
-            }else {
-                document.getElementById(element.id).classList.add('p2');
-                player1 = true;
-            }
-        
-            checkWin();     
-        }
-
+      checkWin();
     }
-
-    
-    
-};
-
-
-
-
-function checkWin() {
-
-    
-    
-    if(i1.classList[1] != undefined  && i1.classList[1] == i2.classList[1] && i1.classList[1] == i3.classList[1]) {
-        i1.classList.add('win');
-        i2.classList.add('win');
-        i3.classList.add('win');
-        
-        playing = false; 
-    }
-
-    if(i4.classList[1] != undefined  && i4.classList[1] == i5.classList[1] && i4.classList[1] == i6.classList[1]) {
-        i4.classList.add('win');
-        i5.classList.add('win');
-        i6.classList.add('win');
-
-        playing = false;
-    }   
-
-    if(i7.classList[1] != undefined  && i7.classList[1] == i8.classList[1] && i7.classList[1] == i9.classList[1]) {
-        i7.classList.add('win');
-        i8.classList.add('win');
-        i9.classList.add('win');
-
-        playing = false;
-    }
-
-    if(i1.classList[1] != undefined  && i1.classList[1] == i4.classList[1] && i1.classList[1] == i7.classList[1]) {
-        i1.classList.add('win');
-        i4.classList.add('win');
-        i7.classList.add('win');
-
-        playing = false;
-    }
-
-    if(i2.classList[1] != undefined  && i2.classList[1] == i5.classList[1] && i2.classList[1] == i8.classList[1]) {
-        i5.classList.add('win');
-        i2.classList.add('win');
-        i8.classList.add('win');
-        playing = false;
-    }
-
-    if(i3.classList[1] != undefined  && i3.classList[1] == i6.classList[1] && i3.classList[1] == i9.classList[1]) {
-        i9.classList.add('win');
-        i6.classList.add('win');
-        i3.classList.add('win');
-
-        playing = false;
-    }
-
-    if(i1.classList[1] != undefined  && i1.classList[1] == i5.classList[1] && i1.classList[1] == i9.classList[1]) {
-        i1.classList.add('win');
-        i5.classList.add('win');
-        i9.classList.add('win');
-
-        playing = false;
-    }
-
-    if(i3.classList[1] != undefined  && i3.classList[1] == i5.classList[1] && i3.classList[1] == i7.classList[1]) {
-        i5.classList.add('win');
-        i7.classList.add('win');
-        i3.classList.add('win');
-
-        playing = false;
-    }
-    
-    
-    
+  }
 }
 
+function checkWin() {
+  if (
+    i1.classList[1] != undefined &&
+    i1.classList[1] == i2.classList[1] &&
+    i1.classList[1] == i3.classList[1]
+  ) {
+    i1.classList.add("win");
+    i2.classList.add("win");
+    i3.classList.add("win");
 
+    playing = false;
+  }
+
+  if (
+    i4.classList[1] != undefined &&
+    i4.classList[1] == i5.classList[1] &&
+    i4.classList[1] == i6.classList[1]
+  ) {
+    i4.classList.add("win");
+    i5.classList.add("win");
+    i6.classList.add("win");
+
+    playing = false;
+  }
+
+  if (
+    i7.classList[1] != undefined &&
+    i7.classList[1] == i8.classList[1] &&
+    i7.classList[1] == i9.classList[1]
+  ) {
+    i7.classList.add("win");
+    i8.classList.add("win");
+    i9.classList.add("win");
+
+    playing = false;
+  }
+
+  if (
+    i1.classList[1] != undefined &&
+    i1.classList[1] == i4.classList[1] &&
+    i1.classList[1] == i7.classList[1]
+  ) {
+    i1.classList.add("win");
+    i4.classList.add("win");
+    i7.classList.add("win");
+
+    playing = false;
+  }
+
+  if (
+    i2.classList[1] != undefined &&
+    i2.classList[1] == i5.classList[1] &&
+    i2.classList[1] == i8.classList[1]
+  ) {
+    i5.classList.add("win");
+    i2.classList.add("win");
+    i8.classList.add("win");
+    playing = false;
+  }
+
+  if (
+    i3.classList[1] != undefined &&
+    i3.classList[1] == i6.classList[1] &&
+    i3.classList[1] == i9.classList[1]
+  ) {
+    i9.classList.add("win");
+    i6.classList.add("win");
+    i3.classList.add("win");
+
+    playing = false;
+  }
+
+  if (
+    i1.classList[1] != undefined &&
+    i1.classList[1] == i5.classList[1] &&
+    i1.classList[1] == i9.classList[1]
+  ) {
+    i1.classList.add("win");
+    i5.classList.add("win");
+    i9.classList.add("win");
+
+    playing = false;
+  }
+
+  if (
+    i3.classList[1] != undefined &&
+    i3.classList[1] == i5.classList[1] &&
+    i3.classList[1] == i7.classList[1]
+  ) {
+    i5.classList.add("win");
+    i7.classList.add("win");
+    i3.classList.add("win");
+
+    playing = false;
+  }
+}
 
 function newSize() {
-    if(window.innerHeight > window.innerWidth) {
-       
-       tictactoe.style.width = '90vw';
-       tictactoe.style.height = '90vw';
-       
-    } else {
-
-        tictactoe.style.width = '90vh';
-        tictactoe.style.height = '90vh';
-        
-    }
-};
-
+  if (window.innerHeight > window.innerWidth) {
+    tictactoe.style.width = "90vw";
+    tictactoe.style.height = "90vw";
+  } else {
+    tictactoe.style.width = "90vh";
+    tictactoe.style.height = "90vh";
+  }
+}
 
 newSize();
 window.addEventListener("resize", newSize);
 
-
 // reset
 function reset() {
+  i1.classList = "gitem";
+  i2.classList = "gitem";
+  i3.classList = "gitem";
+  i4.classList = "gitem";
+  i5.classList = "gitem";
+  i6.classList = "gitem";
+  i7.classList = "gitem";
+  i8.classList = "gitem";
+  i9.classList = "gitem";
 
-    i1.classList = 'gitem'
-    i2.classList = 'gitem'
-    i3.classList = 'gitem'
-    i4.classList = 'gitem'
-    i5.classList = 'gitem'
-    i6.classList = 'gitem'
-    i7.classList = 'gitem'
-    i8.classList = 'gitem'
-    i9.classList = 'gitem'
-    
-
-    playing = true;
-    player1 = true;
-    round = 0;
-
+  playing = true;
+  player1 = true;
+  round = 0;
 }
